ac80e9e5bb0e8f36f224f296b3d302ed993e0310
tom--bo <otsuka.t.2013@gmail.com>
2016-06-19 14:43:42 +0900

    min

--- min.go
+++ min.go
@@ -9,65 +9,82 @@ import (
 
 func main() {
 	//データの検索
-	cnt := 2
-	for cnt < 51 {
+	ti := 0
+	for ti < 500 {
 		db, err := sql.Open("postgres", "user=postgres password=pass dbname=mecab sslmode=disable")
 		checkErr(err)
+		var surface_arr [10]string
+		var original_arr [10]string
+		var reading_arr [10]string
+		var length_arr [10]int
 
-		rows, err := db.Query("SELECT * FROM words WHERE original NOT IN(select original from metadata) LIMIT 100")
+		tx, err := db.Begin()
+
+		if err != nil {
+			fmt.Println("トランザクションの取得に失敗しました。: %v", err)
+		}
+
+		rows, err := db.Query("SELECT surface, original, reading FROM words_queue LIMIT 10 FOR SHARE")
 		checkErr(err)
 
+		cnt := 0
 		for rows.Next() {
 			var surface string
 			var original string
 			var reading string
-			var first_c sql.NullString
-			var last_c sql.NullString
-			var length int
-			var wcnt int
-			err = rows.Scan(&surface, &original, &reading, &first_c, &last_c)
+			err = rows.Scan(&surface, &original, &reading)
 			checkErr(err)
-			/*
-				fmt.Println(original)
-				fmt.Println(surface)
-				fmt.Println(reading)
-					fmt.Println(reading[0:3])
-					fmt.Println(reading[len(reading)-3 : len(reading)])
-					fmt.Println(first_c)
-					fmt.Println(last_c)
-			*/
 
-			length = utf8.RuneCountInString(reading)
+			surface_arr[cnt] = surface
+			original_arr[cnt] = original
+			reading_arr[cnt] = reading
+			length_arr[cnt] = utf8.RuneCountInString(reading)
+			cnt++
+		}
 
-			if length < 3 {
-				// insert
-				//データの挿入
-				stmt, err := db.Prepare("INSERT INTO metadata(original, same_reading_count, minimum_length) VALUES($1,$2,$3)")
+		for i := 0; i < 10; i++ {
+			query := "DELETE FROM words_queue WHERE original = $1"
+			_, err := tx.Exec(query, original_arr[i])
+			checkErr(err)
+		}
+
+		// 本来ならerrの内容を確認してcommitまたはrollbackを決める必要がある
+		err = tx.Commit()
+
+		if err != nil {
+			fmt.Println("トランザクションのコミットに失敗しました。: %v", err)
+			return
+		}
+
+		for j := 0; j < 10; j++ {
+			if length_arr[j] < 3 {
+				stmt, err := db.Prepare("INSERT INTO metadata(original, minimum_length) VALUES($1,$2)")
 				checkErr(err)
 
-				_, err = stmt.Exec(original, length, length)
+				_, err = stmt.Exec(original_arr[j], length_arr[j])
 				checkErr(err)
 			} else {
-				tmp_len := length
+				tmp_len := length_arr[j]
 
 				for tmp_len >= 2 {
-					row, err := db.Query("SELECT count(*) FROM words WHERE reading LIKE $1", reading[0:tmp_len*3]+"%")
+					var wcnt int
+					row, err := db.Query("SELECT count(*) FROM words WHERE reading LIKE $1", reading_arr[j][0:tmp_len*3]+"%")
 					for row.Next() {
 						err = row.Scan(&wcnt)
 						checkErr(err)
 
-						if wcnt >= 2 && tmp_len == length {
-							stmt, err := db.Prepare("INSERT INTO metadata(original, same_reading_count, minimum_length) VALUES($1,$2,$3)")
+						if wcnt >= 2 && tmp_len == length_arr[j] {
+							stmt, err := db.Prepare("INSERT INTO metadata(original, minimum_length) VALUES($1,$2)")
 							checkErr(err)
 
-							_, err = stmt.Exec(original, wcnt, length)
+							_, err = stmt.Exec(original_arr[j], length_arr[j])
 							checkErr(err)
 							break
 						} else if wcnt >= 2 {
-							stmt, err := db.Prepare("INSERT INTO metadata(original, same_reading_count, minimum_length) VALUES($1,$2,$3)")
+							stmt, err := db.Prepare("INSERT INTO metadata(original, minimum_length) VALUES($1,$2)")
 							checkErr(err)
 
-							_, err = stmt.Exec(original, 1, tmp_len+1)
+							_, err = stmt.Exec(original_arr[j], tmp_len+1)
 							checkErr(err)
 							break
 						}
@@ -79,11 +96,12 @@ func main() {
 				}
 			}
 		}
-		cnt += 1
-		if cnt%50 == 0 {
-			fmt.Println(cnt)
+		ti += 1
+		if ti%50 == 0 {
+			fmt.Println(ti)
 			break
 		}
+
 		db.Close()
 	}
 	fmt.Println("finish")
